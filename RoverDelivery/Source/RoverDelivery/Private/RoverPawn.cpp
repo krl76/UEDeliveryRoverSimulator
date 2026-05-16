e661f20485ec2c4695a4d4dd8ef0abcc7ea17430
krl76 <99721154+krl76@users.noreply.github.com>
2026-05-17 01:08:03 +0300

    new proj

---
+++ RoverDelivery/Source/RoverDelivery/Private/RoverPawn.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RoverDelivery/Public/RoverPawn.h"
+
+
+// Sets default values
+ARoverPawn::ARoverPawn()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ARoverPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARoverPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ARoverPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
