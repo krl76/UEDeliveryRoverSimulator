5a5489b2cf10b24a2ff19fd9707468efb9311a49
krl76 <99721154+krl76@users.noreply.github.com>
2026-05-17 22:08:55 +0300

    simple movement

--- RoverDelivery/Source/RoverDelivery/Private/RoverPawn.cpp
+++ RoverDelivery/Source/RoverDelivery/Private/RoverPawn.cpp
@@ -1,32 +1,255 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "RoverPawn.h"
 
+#include "Camera/CameraComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
 
-#include "RoverDelivery/Public/RoverPawn.h"
-
-
-// Sets default values
 ARoverPawn::ARoverPawn()
 {
-	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = true;
+
+    CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+    RootComponent = CollisionBox;
+
+    CollisionBox->SetBoxExtent(FVector(80.0f, 55.0f, 35.0f));
+    CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+    CollisionBox->SetCollisionObjectType(ECC_Pawn);
+    CollisionBox->SetCollisionResponseToAllChannels(ECR_Block);
+
+    RoverMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RoverMesh"));
+    RoverMesh->SetupAttachment(CollisionBox);
+    RoverMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+    SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+    SpringArm->SetupAttachment(CollisionBox);
+    SpringArm->TargetArmLength = 500.0f;
+    SpringArm->SetRelativeLocation(FVector(0.0f, 0.0f, 80.0f));
+    SpringArm->bUsePawnControlRotation = false;
+    SpringArm->bEnableCameraLag = true;
+    SpringArm->CameraLagSpeed = 8.0f;
+
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    Camera->SetupAttachment(SpringArm);
+    Camera->bUsePawnControlRotation = false;
+
+    AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
-// Called when the game starts or when spawned
 void ARoverPawn::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+
+    ApplyCameraRotation();
+
+    APlayerController* PlayerController = Cast<APlayerController>(GetController());
+    if (!PlayerController)
+    {
+        return;
+    }
+
+    ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer();
+    if (!LocalPlayer)
+    {
+        return;
+    }
+
+    UEnhancedInputLocalPlayerSubsystem* InputSubsystem =
+        LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
+
+    if (InputSubsystem && RoverMappingContext)
+    {
+        InputSubsystem->AddMappingContext(RoverMappingContext, 0);
+    }
 }
 
-// Called every frame
 void ARoverPawn::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
+
+    MoveRover(DeltaTime);
+    RotateRover(DeltaTime);
 }
 
-// Called to bind functionality to input
 void ARoverPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+    if (!EnhancedInputComponent)
+    {
+        return;
+    }
+
+    if (ThrottleAction)
+    {
+        EnhancedInputComponent->BindAction(ThrottleAction, ETriggerEvent::Triggered, this, &ARoverPawn::HandleThrottle);
+        EnhancedInputComponent->BindAction(ThrottleAction, ETriggerEvent::Completed, this, &ARoverPawn::HandleThrottleCompleted);
+    }
+
+    if (SteerAction)
+    {
+        EnhancedInputComponent->BindAction(SteerAction, ETriggerEvent::Triggered, this, &ARoverPawn::HandleSteer);
+        EnhancedInputComponent->BindAction(SteerAction, ETriggerEvent::Completed, this, &ARoverPawn::HandleSteerCompleted);
+    }
+
+    if (BoostAction)
+    {
+        EnhancedInputComponent->BindAction(BoostAction, ETriggerEvent::Started, this, &ARoverPawn::HandleBoostStarted);
+        EnhancedInputComponent->BindAction(BoostAction, ETriggerEvent::Completed, this, &ARoverPawn::HandleBoostCompleted);
+    }
+
+    if (BrakeAction)
+    {
+        EnhancedInputComponent->BindAction(BrakeAction, ETriggerEvent::Started, this, &ARoverPawn::HandleBrakeStarted);
+        EnhancedInputComponent->BindAction(BrakeAction, ETriggerEvent::Completed, this, &ARoverPawn::HandleBrakeCompleted);
+    }
+
+    if (LookXAction)
+    {
+        EnhancedInputComponent->BindAction(LookXAction, ETriggerEvent::Triggered, this, &ARoverPawn::HandleLookX);
+    }
+
+    if (LookYAction)
+    {
+        EnhancedInputComponent->BindAction(LookYAction, ETriggerEvent::Triggered, this, &ARoverPawn::HandleLookY);
+    }
+
+    if (InteractAction)
+    {
+        EnhancedInputComponent->BindAction(InteractAction, ETriggerEvent::Started, this, &ARoverPawn::HandleInteractStarted);
+    }
+
+    if (PauseAction)
+    {
+        EnhancedInputComponent->BindAction(PauseAction, ETriggerEvent::Started, this, &ARoverPawn::HandlePauseStarted);
+    }
+
+    if (CameraToggleAction)
+    {
+        EnhancedInputComponent->BindAction(CameraToggleAction, ETriggerEvent::Started, this, &ARoverPawn::HandleCameraToggleStarted);
+    }
 }
 
+void ARoverPawn::HandleThrottle(const FInputActionValue& Value)
+{
+    ThrottleValue = Value.Get<float>();
+}
+
+void ARoverPawn::HandleThrottleCompleted(const FInputActionValue& Value)
+{
+    ThrottleValue = 0.0f;
+}
+
+void ARoverPawn::HandleSteer(const FInputActionValue& Value)
+{
+    SteerValue = Value.Get<float>();
+}
+
+void ARoverPawn::HandleSteerCompleted(const FInputActionValue& Value)
+{
+    SteerValue = 0.0f;
+}
+
+void ARoverPawn::HandleBoostStarted()
+{
+    bBoosting = true;
+}
+
+void ARoverPawn::HandleBoostCompleted()
+{
+    bBoosting = false;
+}
+
+void ARoverPawn::HandleBrakeStarted()
+{
+    bBraking = true;
+}
+
+void ARoverPawn::HandleBrakeCompleted()
+{
+    bBraking = false;
+}
+
+void ARoverPawn::HandleLookX(const FInputActionValue& Value)
+{
+    const float LookAmount = Value.Get<float>();
+
+    CameraYaw += LookAmount * LookSensitivity;
+    ApplyCameraRotation();
+}
+
+void ARoverPawn::HandleLookY(const FInputActionValue& Value)
+{
+    const float LookAmount = Value.Get<float>();
+
+    CameraPitch += LookAmount * LookSensitivity;
+    CameraPitch = FMath::Clamp(CameraPitch, MinCameraPitch, MaxCameraPitch);
+
+    ApplyCameraRotation();
+}
+
+void ARoverPawn::HandleInteractStarted()
+{
+    OnInteractPressed();
+}
+
+void ARoverPawn::HandlePauseStarted()
+{
+    OnPausePressed();
+}
+
+void ARoverPawn::HandleCameraToggleStarted()
+{
+    OnCameraTogglePressed();
+}
+
+void ARoverPawn::MoveRover(float DeltaTime)
+{
+    if (FMath::IsNearlyZero(ThrottleValue))
+    {
+        return;
+    }
+
+    float CurrentSpeed = MoveSpeed;
+
+    if (bBoosting)
+    {
+        CurrentSpeed = BoostSpeed;
+    }
+
+    if (bBraking)
+    {
+        CurrentSpeed *= BrakeMultiplier;
+    }
+
+    const FVector ForwardDirection = GetActorForwardVector();
+    const FVector DeltaLocation = ForwardDirection * ThrottleValue * CurrentSpeed * DeltaTime;
+
+    AddActorWorldOffset(DeltaLocation, true);
+}
+
+void ARoverPawn::RotateRover(float DeltaTime)
+{
+    if (FMath::IsNearlyZero(SteerValue))
+    {
+        return;
+    }
+
+    const float DeltaYaw = SteerValue * TurnSpeed * DeltaTime;
+    const FRotator DeltaRotation = FRotator(0.0f, DeltaYaw, 0.0f);
+
+    AddActorLocalRotation(DeltaRotation, true);
+}
+
+void ARoverPawn::ApplyCameraRotation()
+{
+    if (SpringArm)
+    {
+        SpringArm->SetRelativeRotation(FRotator(CameraPitch, CameraYaw, 0.0f));
+    }
+}
--- RoverDelivery/Source/RoverDelivery/Public/RoverPawn.h
+++ RoverDelivery/Source/RoverDelivery/Public/RoverPawn.h
@@ -1,28 +1,139 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "RoverPawn.generated.h"
+
+class UBoxComponent;
+class UStaticMeshComponent;
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class ROVERDELIVERY_API ARoverPawn : public APawn
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
-	ARoverPawn();
+    ARoverPawn();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
 public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Rover|Components")
+    UBoxComponent* CollisionBox;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Rover|Components")
+    UStaticMeshComponent* RoverMesh;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Rover|Components")
+    USpringArmComponent* SpringArm;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Rover|Components")
+    UCameraComponent* Camera;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rover|Input")
+    UInputMappingContext* RoverMappingContext;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rover|Input")
+    UInputAction* ThrottleAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rover|Input")
+    UInputAction* SteerAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rover|Input")
+    UInputAction* BoostAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rover|Input")
+    UInputAction* BrakeAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rover|Input")
+    UInputAction* InteractAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rover|Input")
+    UInputAction* PauseAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rover|Input")
+    UInputAction* CameraToggleAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rover|Input")
+    UInputAction* LookXAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rover|Input")
+    UInputAction* LookYAction;
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rover|Movement")
+    float MoveSpeed = 500.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rover|Movement")
+    float BoostSpeed = 900.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rover|Movement")
+    float BrakeMultiplier = 0.25f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rover|Movement")
+    float TurnSpeed = 100.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rover|Camera")
+    float LookSensitivity = 0.7f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rover|Camera")
+    float MinCameraPitch = -70.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rover|Camera")
+    float MaxCameraPitch = -5.0f;
+
+private:
+    float ThrottleValue = 0.0f;
+    float SteerValue = 0.0f;
+
+    bool bBoosting = false;
+    bool bBraking = false;
+
+    float CameraYaw = 0.0f;
+    float CameraPitch = -25.0f;
+
+private:
+    void HandleThrottle(const FInputActionValue& Value);
+    void HandleThrottleCompleted(const FInputActionValue& Value);
+
+    void HandleSteer(const FInputActionValue& Value);
+    void HandleSteerCompleted(const FInputActionValue& Value);
+
+    void HandleBoostStarted();
+    void HandleBoostCompleted();
+
+    void HandleBrakeStarted();
+    void HandleBrakeCompleted();
+
+    void HandleLookX(const FInputActionValue& Value);
+    void HandleLookY(const FInputActionValue& Value);
+
+    void HandleInteractStarted();
+    void HandlePauseStarted();
+    void HandleCameraToggleStarted();
+
+    void MoveRover(float DeltaTime);
+    void RotateRover(float DeltaTime);
+    void ApplyCameraRotation();
+
+protected:
+    UFUNCTION(BlueprintImplementableEvent, Category = "Rover|Events")
+    void OnInteractPressed();
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "Rover|Events")
+    void OnPausePressed();
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "Rover|Events")
+    void OnCameraTogglePressed();
 };
